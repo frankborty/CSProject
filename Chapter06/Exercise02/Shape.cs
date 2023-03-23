9173283973c2ca63a9281123d26bf14042b8629c
frankborty <dessena.franco@gmail.com>
2023-03-23 13:50:53 +0100

    Aggiunti tutti i progetti a Chapter06

---
+++ Chapter06/Exercise02/Shape.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{
+    public abstract class Shape
+    {
+        public virtual double Height { get; set; }
+        public virtual double Width { get; set; }
+        public virtual double Area { get; set; }
+    }
+
+    public class Rectangle : Shape
+    {
+        public Rectangle(double height, double width) {
+            Height = height;
+            Width = width;
+        }
+
+        public override double Area { get => base.Height * base.Width; }
+
+    }
+
+    public class Square : Shape
+    {
+        public Square(double v) {
+            Height = v;
+            Width = v;
+        }
+
+        public override double Width
+        {
+            get => base.Width;
+            set
+            {
+                base.Width = value;
+                base.Height = value;
+            }
+        }
+
+        public override double Height
+        {
+            get => base.Height;
+            set
+            {
+                base.Width = value;
+                base.Height = value;
+            }
+        }
+
+        public override double Area { get => base.Height * base.Height; }
+
+    }
+
+    public class Circle : Shape
+    {
+        public Circle(double radius)
+        {
+            Width = radius;
+            Height = radius;
+        }
+
+        public override double Width
+        {
+            get => base.Width;
+            set
+            {
+                base.Width = value;
+                base.Height = value;
+            }
+        }
+
+        public override double Height
+        {
+            get => base.Height;
+            set
+            {
+                base.Width = value;
+                base.Height = value;
+            }
+        }
+
+        public override double Area { get => Math.Pow(base.Height,2) * Math.PI; }
+
+    }
+
+}
