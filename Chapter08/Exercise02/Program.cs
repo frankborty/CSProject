09cf956471aced59193c6ef16c05e3d984353489
frankborty <dessena.franco@gmail.com>
2023-03-27 20:05:34 +0200

    Aggiunti tutti i progetti a Chapter08

---
+++ Chapter08/Exercise02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+using static System.Console;
+
+WriteLine("The default regular expression checks for at least one digit.");
+
+
+while (true)
+{
+    WriteLine("Enter a regular expression (or press ENTER to use the default): ^[a-z]+$");
+    string? inputRegex = ReadLine();
+    if (string.IsNullOrWhiteSpace(inputRegex))
+    {
+        inputRegex = @"^\d+$";
+    }
+
+    WriteLine("Enter some input:");
+    string? inputString = ReadLine();
+
+    Regex r = new(inputRegex);
+
+    WriteLine($"{inputString} matches ${inputRegex} : {r.IsMatch(inputString)}");
+
+    WriteLine("Press ESC to end or any key to try again.");
+    if (ReadKey().Key == ConsoleKey.Escape)
+    {
+        return;
+    }
+}
